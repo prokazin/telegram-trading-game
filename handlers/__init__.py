a7190fcaf9f53dee1e78d03993df8abf2c95f2e4
prokazin <nazar.bronnikov22@gmail.com>
2025-12-04 21:55:15 +0900

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,7 @@
+from .start import StartHandler
+from .trading import TradingHandler
+from .portfolio import PortfolioHandler
+from .chart import ChartHandler
+from .admin import AdminHandler
+
+__all__ = ['StartHandler', 'TradingHandler', 'PortfolioHandler', 'ChartHandler', 'AdminHandler']
